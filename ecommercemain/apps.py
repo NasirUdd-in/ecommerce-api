34eda9cc47f01c4638b281f802f774964101c745
Nasirudd-in <nasir.diudevs@gmail.com>
2024-01-27 12:54:41 +0600

    first commit

---
+++ ecommercemain/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcommercemainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ecommercemain'
---
+++ ecommercemain/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from datetime import date
+
+# class User(AbstractUser):
+#   username = models.CharField(max_length = 50, blank = True, null = True, unique = True)
+
+ 
